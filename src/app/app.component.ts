16f21239aa839c929fa0800b91bd3cabfa3f3c27
himanshu@webtheuniverse.com <himanshu@webtheuniverse.com>
2022-08-10 18:55:45 +0530

    continue in starwars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,72 @@
-import { getLocaleMonthNames } from '@angular/common';
-import { Component } from '@angular/core';
-import { subscribeOn } from 'rxjs';
-import { ApidataService } from './services/apidata.service';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent {
   title = 'starwars';
   peopleM: any[] = [];
-  people: string = '';
-  url='https://starwars-visualguide.com/assets/img/characters';
-  tempUrl='';
-  images:any[]=[];
-  
-  constructor(private apidata: ApidataService) {
-    console.log('apidata', apidata);
-    
-    for (let i = 1; i <= 9; i++) {
-          apidata.peoples(i).subscribe((data: any) => {
-          console.log("data",data.results);
-          this.peopleM.push(data.results);
-          console.log('i', i);
-        });             
-    }
+  url = 'https://starwars-visualguide.com/assets/img/characters';
+  url2 = 'https://swapi.dev/api/people';
+  tempUrl = '';
+  images: any[] = [];
+  tempImages: any[] = [];
+  color:string='red';
 
-    for(let i = 1; i <= 83; i++) {
-      if(i !== 17) {
-        this.tempUrl = `${this.url}/${i}${'.jpg'}`;
-        this.images.push(this.tempUrl);
-      }
-    }
+
+  constructor(private http:HttpClient) {
+    // for(let i = 1; i <= 83; i++) {
+    //   if(i !== 17) {
+    //     this.tempUrl = `${this.url}/${i}${'.jpg'}`;
+    //     this.images.push(this.tempUrl);
+    //   }
+    // }
   }
+  async ngOnInit() {
+    for (let i = 1; i <= 9; i++) {
+      let tempUrl='';
+      tempUrl = `${this.url2}/${'?page='}${i}`;
+
+      this.http.get<any>(tempUrl).subscribe(data => {
+        this.peopleM.push(data.results); 
+      })
+      await this.http.get<any>(tempUrl).toPromise().then(data => {
+      })
+    }
+    
+  }
+
+
+
+  // constructor(private apidata: ApidataService) {
+  //   // console.log('apidata', apidata);
+  //   // for (let i = 1; i <= 9; i++) {
+  //   //   apidata.peoples(i).subscribe((data: any) => {
+  //   //     console.log("data",data.results);
+  //   //     this.peopleM.push(data.results);
+  //   //     console.log('i', i);
+  //   //   });
+  //   // }
+
+  //   // this.temppeopleM = this.peopleM;
+   
+  //   // this.tempImages = this.images;
+  // }
+  
+  // async await
+
+  // async getData(url2: string) {
+  //   let data = await this.apidata.getDataSynchronous(url2);
+  //   console.log('data', data.results);
+  //   this.peopleM.push(data.results);  
+  // }
+  // ngOnInit(): void {
+  //   for (let i = 1; i <= 9; i++) {
+  //     this.tempUrl = `${this.url2}/${'?page='}${i}`;
+  //     this.getData(this.tempUrl);
+  //   }
+  // }
 }
