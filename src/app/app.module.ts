1cd76b2e1faa54b47517df8199dfba92bdc5da08
himanshu@webtheuniverse.com <himanshu@webtheuniverse.com>
2022-08-12 19:02:26 +0530

    starwars continue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CharlistComponent } from './charlist/charlist.component';
+import { ChardetailComponent } from './chardetail/chardetail.component';
+
 @NgModule({
   declarations: [
     AppComponent,
+    CharlistComponent,
+    ChardetailComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/backUp.ts
+++ src/app/backUp.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient  } from '@angular/common/http';
+import { Component , ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'starwars';
+  urlI = 'https://starwars-visualguide.com/assets/img/characters';
+  url = 'https://swapi.dev/api/people';
+  tempUrl = '';
+  count: number = 1;
+  pageCount:number = 1;
+  ok:boolean=true;
+
+  constructor(private http: HttpClient, private elementRef: ElementRef) {}
+
+  async ngOnInit() {
+    const element = this.elementRef.nativeElement.querySelector('.main_div');
+    for (let i = 1; i <= 9; i++) {
+      let tempUrl = '';
+      tempUrl = `${this.url}/${'?page='}${this.pageCount}`;
+      this.pageCount++;
   
+      await this.http.get<any>(tempUrl).toPromise().then(data => {
+        data.results.forEach((ele: any) => {
+          let html = '';
+          if(this.count != 17) {
+            html = `
+            <div class="detail">
+               <img src="https://starwars-visualguide.com/assets/img/characters/${this.count}.jpg" alt="No image found">
+               <h2>${ele.name}</h2>
+            </div>
+            `;
+          }
+          else {
+            this.count++;
+            html = `
+            <div class="detail">
+               <img src="https://starwars-visualguide.com/assets/img/characters/${this.count}.jpg" alt="No image found">
+               <h2>${ele.name}</h2>
+            </div>
+            `;
+          }
+          element.insertAdjacentHTML('beforeend', html);
+          this.count++;
+        })
+        console.log("data",data.results);
+      })
+    }
+  }
   /* async ngOnInit() {
     const element = this.elementRef.nativeElement.querySelector('.main_div');
     for(let i = 1; i <= 9; i++) {
